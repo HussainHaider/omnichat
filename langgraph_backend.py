584a830875e0b82a4086c20b40663ec9c5582679
HussainHaider <hussainhaider490@gmail.com>
2026-05-30 08:43:31 +0200

    Enhance database integration by adding SQLite support and updating thread management in backend API

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,10 +1,11 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -18,8 +19,10 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+sqlite_path = "chatbot.db"
+conn = sqlite3.connect(sqlite_path, check_same_thread=False)
 # Checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -27,3 +30,15 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# test
+# CONFIG = {"configurable": {"thread_id": "chat_thread_2"}}
+# initial_state = {"messages": [HumanMessage(content="What is my name?")]}
+# final_state = chatbot.invoke(initial_state, config=CONFIG)
+# print(final_state['messages'][-1].content)
+
+def get_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+    return list(all_threads)
